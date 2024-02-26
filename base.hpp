566664e24ac374afc0d2b172c5c6167dcdc6e518
shogo314 <shg.kawasome@gmail.com>
2024-02-26 11:26:08 +0900

    rename

--- base.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <bits/stdc++.h>
-
-#include "base/functions.hpp"
-#include "base/macro.hpp"
-#include "base/traits.hpp"
-#include "base/type_alias.hpp"
-#include "base/vector_func.hpp"
