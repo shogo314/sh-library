3ed52fd7aab525c9f778fc17de2a393681fc4a2d
shogo314 <shg.kawasome@gmail.com>
2024-03-06 04:05:47 +0900

    add func

--- src/base/vector_func.hpp
+++ src/base/vector_func.hpp
@@ -15,14 +15,8 @@ inline T min(const std::vector<T> &v) {
 }
 
 template <typename T>
-inline std::vector<T> sorted(const std::vector<T> &v) {
+inline std::vector<T> sorted(std::vector<T> v) {
     std::sort(v.begin(), v.end());
-    return v;
-}
-
-template <typename T>
-inline std::vector<T> reversed(const std::vector<T> &v) {
-    std::reverse(v.begin(), v.end());
     return v;
 }
 
@@ -40,7 +34,7 @@ inline T product(const std::vector<T> &v, T init = {1}) {
 }
 
 template <typename T>
-T gcd(const std::vector<T> &v, T init = {0}) {
+inline T gcd(const std::vector<T> &v, T init = {0}) {
     for (const T &e : v) init = std::gcd(init, e);
     return init;
 }
@@ -70,31 +64,49 @@ inline size_t index(const std::vector<T> &v, const U &x) {
     return std::distance(v.begin(), std::find(v.begin(), v.end(), T(x)));
 }
 
+template <typename T, std::enable_if_t<std::is_integral_v<T>, std::nullptr_t> = nullptr>
+inline T mex(const std::vector<T> &v) {
+    std::vector<bool> b(v.size() + 1);
+    for (const auto &a : v) {
+        if (0 <= a and a < b.size()) {
+            b[a] = true;
+        }
+    }
+    T ret;
+    for (size_t i = 0; i < b.size(); i++) {
+        if (not b[i]) {
+            ret = i;
+            break;
+        }
+    }
+    return ret;
+}
+
 template <typename T>
-std::vector<T> &operator++(std::vector<T> &v) {
+inline std::vector<T> &operator++(std::vector<T> &v) {
     for (auto &e : v) e++;
     return v;
 }
 template <typename T>
-std::vector<T> operator++(std::vector<T> &v, int) {
+inline std::vector<T> operator++(std::vector<T> &v, int) {
     auto res = v;
     for (auto &e : v) e++;
     return res;
 }
 template <typename T>
-std::vector<T> &operator--(std::vector<T> &v) {
+inline std::vector<T> &operator--(std::vector<T> &v) {
     for (auto &e : v) e--;
     return v;
 }
 template <typename T>
-std::vector<T> operator--(std::vector<T> &v, int) {
+inline std::vector<T> operator--(std::vector<T> &v, int) {
     auto res = v;
     for (auto &e : v) e--;
     return res;
 }
 
-template <typename T, typename U, std::enable_if_t<is_addible_with_v<T, U>, std::nullptr_t> = nullptr>
-std::vector<T> &operator+=(std::vector<T> &v1, const std::vector<U> &v2) {
+template <typename T, typename U, std::enable_if_t<std::is_convertible_v<U, T>, std::nullptr_t> = nullptr>
+inline std::vector<T> &operator+=(std::vector<T> &v1, const std::vector<U> &v2) {
     if (v2.size() > v1.size()) {
         v1.resize(v2.size());
     }
@@ -104,28 +116,113 @@ std::vector<T> &operator+=(std::vector<T> &v1, const std::vector<U> &v2) {
     return v1;
 }
 
-template <typename T, typename U, std::enable_if_t<is_addible_with_v<T, U>, std::nullptr_t> = nullptr>
-std::vector<T> &operator+=(std::vector<T> &v, const U &u) {
+template <typename T, typename U, std::enable_if_t<std::is_convertible_v<U, T>, std::nullptr_t> = nullptr>
+inline std::vector<T> operator+(const std::vector<T> &v1, const std::vector<U> &v2) {
+    std::vector<T> res(v1);
+    return res += v2;
+}
+
+template <typename T, typename U, std::enable_if_t<std::is_convertible_v<U, T>, std::nullptr_t> = nullptr>
+inline std::vector<T> &operator+=(std::vector<T> &v, const U &u) {
     for (T &e : v) {
         e += u;
     }
     return v;
 }
 
+template <typename T, typename U, std::enable_if_t<std::is_convertible_v<U, T>, std::nullptr_t> = nullptr>
+inline std::vector<T> operator+(const std::vector<T> &v, const U &u) {
+    std::vector<T> res(v);
+    return res += u;
+}
+
+template <typename T, typename U, std::enable_if_t<std::is_convertible_v<U, T>, std::nullptr_t> = nullptr>
+inline std::vector<T> operator+(const U &u, const std::vector<T> &v) {
+    std::vector<T> res(v);
+    return res += u;
+}
+
+template <typename T, typename U, std::enable_if_t<std::is_convertible_v<U, T>, std::nullptr_t> = nullptr>
+inline std::vector<T> &operator*=(std::vector<T> &v1, const std::vector<U> &v2) {
+    if (v2.size() > v1.size()) {
+        v1.resize(v2.size());
+    }
+    for (size_t i = 0; i < v2.size(); i++) {
+        v1[i] *= v2[i];
+    }
+    for (size_t i = v2.size(); i < v1.size(); i++) {
+        v1[i] *= U(0);
+    }
+    return v1;
+}
+
+template <typename T, typename U, std::enable_if_t<std::is_convertible_v<U, T>, std::nullptr_t> = nullptr>
+inline std::vector<T> operator*(const std::vector<T> &v1, const std::vector<U> &v2) {
+    std::vector<T> res(v1);
+    return res *= v2;
+}
+
+template <typename T, typename U, std::enable_if_t<std::is_convertible_v<U, T>, std::nullptr_t> = nullptr>
+inline std::vector<T> &operator*=(std::vector<T> &v, const U &u) {
+    for (T &e : v) {
+        e *= u;
+    }
+    return v;
+}
+
+template <typename T, typename U, std::enable_if_t<std::is_convertible_v<U, T>, std::nullptr_t> = nullptr>
+inline std::vector<T> operator*(const std::vector<T> &v, const U &u) {
+    std::vector<T> res(v);
+    return res *= u;
+}
+
+template <typename T, typename U, std::enable_if_t<std::is_convertible_v<U, T>, std::nullptr_t> = nullptr>
+inline std::vector<T> operator*(const U &u, const std::vector<T> &v) {
+    std::vector<T> res(v);
+    return res *= u;
+}
+
 template <typename T, typename U>
-std::vector<T> &assign(std::vector<T> &v1, const std::vector<U> &v2) {
+inline std::vector<T> &assign(std::vector<T> &v1, const std::vector<U> &v2) {
     v1.assign(v2.begin(), v2.end());
     return v1;
 }
 
 template <typename T, typename U>
-std::vector<T> &extend(std::vector<T> &v1, const std::vector<U> &v2) {
+inline std::vector<T> &extend(std::vector<T> &v1, const std::vector<U> &v2) {
     v1.insert(v1.end(), v2.begin(), v2.end());
     return v1;
 }
 
+template <typename T, typename U, std::enable_if_t<std::is_convertible_v<T, U>, std::nullptr_t> = nullptr>
+inline std::vector<T> &operator|=(std::vector<T> &v1, const std::vector<U> &v2) {
+    return extend(v1, v2);
+}
+
+template <typename T, typename U, std::enable_if_t<std::is_integral_v<U>, std::nullptr_t> = nullptr>
+inline std::vector<T> &operator|=(std::vector<T> &v, const U &u) {
+    std::vector<T> w(v);
+    v.clear();
+    for (int i = 0; i < u; i++) {
+        extend(v, w);
+    }
+    return v;
+}
+
+template <typename T, typename U, std::enable_if_t<std::is_integral_v<U>, std::nullptr_t> = nullptr>
+inline std::vector<T> operator|(const std::vector<T> &v, const U &u) {
+    std::vector<T> res(v);
+    return res |= u;
+}
+
+template <typename T, typename U, std::enable_if_t<std::is_integral_v<U>, std::nullptr_t> = nullptr>
+inline std::vector<T> operator|(const U &u, const std::vector<T> &v) {
+    std::vector<T> res(v);
+    return res |= u;
+}
+
 template <typename T>
-std::vector<T> abs(const std::vector<T> &v) {
+inline std::vector<T> abs(const std::vector<T> &v) {
     std::vector<T> ret;
     ret.reserve(v.size());
     for (const T &e : v) ret.push_back(std::abs(e));
