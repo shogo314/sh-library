4572084d6cde6feea8ce6317212cff12c456682b
shogo314 <shg.kawasome@gmail.com>
2024-03-06 23:16:12 +0900

    fix-mstree

--- src/data_structures/merge-sort-tree.hpp
+++ src/data_structures/merge-sort-tree.hpp
@@ -1,24 +1,35 @@
 #pragma once
 #include <algorithm>
 #include <cassert>
+#include <functional>
 #include <optional>
 #include <utility>
 #include <vector>
 
 class MergeSortTree {
-    using S = long long;
     using K = int;
-    static S inv(const S& a) {
-        return -a;
-    }
-    static S op(const S& a, const S& b) {
-        return a + b;
-    }
-    static S e() {
-        return 0;
-    }
+    using S = long long;
+
+    struct Element {
+        S operator()() const {
+            return {};
+        }
+    };
+
+   public:
+    using Compare = std::less<K>;
+    using Operator = std::plus<S>;
+    using Inverse = std::negate<S>;
+    using key_type = K;
+    using value_type = S;
+
+    inline constexpr static auto comp = Compare();
+    inline constexpr static auto op = Operator();
+    inline constexpr static auto inv = Inverse();
+    inline constexpr static auto e = Element();
+
     int n, sz, height;
-    std::vector<std::pair<K, S>> key_value_data;
+    std::vector<K> key_data;
     std::vector<S> cumulative_value;
 
     void initialize(const std::vector<K>& key, const std::vector<S>& value) {
@@ -29,70 +40,72 @@ class MergeSortTree {
             sz <<= 1;
             height++;
         }
-        key_value_data.assign(sz * height, {{}, e()});
+        key_data.assign(sz * height, {});
+        std::vector<S> value_data(sz * height, e());
         cumulative_value.assign(sz * height, {});
         for (int i = 0; i < n; i++) {
-            key_value_data[i + sz * (height - 1)] = {key[i], value[i]};
-            cumulative_value[i + sz * (height - 1)] = value[i];
+            key_data[(height - 1) * sz + i] = key[i];
+            value_data[(height - 1) * sz + i] = value[i];
+            cumulative_value[(height - 1) * sz + i] = value[i];
         }
         int t = 1;
-        // std::cout << "key_value_data[" << (height - 1) * sz << ":" << height * sz << "] =";
-        // for (int i = 0; i < sz; i++) {
-        //     std::cout << " {" << key_value_data[(height - 1) * sz + i].first << ", " << key_value_data[(height - 1) * sz + i].second << "}";
-        // }
-        // std::cout << std::endl;
-        // std::cout << "cumulative_value[" << (height - 1) * sz << ":" << height * sz << "] =";
-        // for (int i = 0; i < sz; i++) {
-        //     std::cout << " " << cumulative_value[(height - 1) * sz + i];
-        // }
-        // std::cout << std::endl;
         for (int h = height - 1; h > 0; h--) {
             for (int i = 0; i < n; i += t * 2) {
-                std::merge(key_value_data.begin() + h * sz + i,
-                           key_value_data.begin() + h * sz + std::min(n, i + t),
-                           key_value_data.begin() + h * sz + std::min(n, i + t),
-                           key_value_data.begin() + h * sz + std::min(n, i + t * 2),
-                           key_value_data.begin() + (h - 1) * sz + i,
-                           [](const std::pair<K, S>& p1, const std::pair<K, S>& p2) { return p1.first < p2.first; });
+                int j1 = h * sz + i;
+                int j2 = h * sz + std::min(n, i + t);
+                int j0 = (h - 1) * sz + i;
+                int last1 = h * sz + std::min(n, i + t);
+                int last2 = h * sz + std::min(n, i + t * 2);
+                while (j1 != last1 or j2 != last2) {
+                    if (j1 == last1) {
+                        key_data[j0] = key_data[j2];
+                        value_data[j0] = value_data[j2];
+                        j0++;
+                        j2++;
+                    } else if (j2 == last2) {
+                        key_data[j0] = key_data[j1];
+                        value_data[j0] = value_data[j1];
+                        j0++;
+                        j1++;
+                    } else if (comp(key_data[j2], key_data[j1])) {
+                        key_data[j0] = key_data[j2];
+                        value_data[j0] = value_data[j2];
+                        j0++;
+                        j2++;
+                    } else {
+                        key_data[j0] = key_data[j1];
+                        value_data[j0] = value_data[j1];
+                        j0++;
+                        j1++;
+                    }
+                }
+
                 if (i < n) {
-                    cumulative_value[(h - 1) * sz + i] = key_value_data[(h - 1) * sz + i].second;
+                    cumulative_value[(h - 1) * sz + i] = value_data[(h - 1) * sz + i];
                 }
                 for (int j = i + 1; j < std::min(n, i + t * 2); j++) {
-                    cumulative_value[(h - 1) * sz + j] = op(cumulative_value[(h - 1) * sz + j - 1], key_value_data[(h - 1) * sz + j].second);
+                    cumulative_value[(h - 1) * sz + j] = op(cumulative_value[(h - 1) * sz + j - 1], value_data[(h - 1) * sz + j]);
                 }
             }
-            // std::cout << "key_value_data[" << (h - 1) * sz << ":" << h * sz << "] =";
-            // for (int i = 0; i < sz; i++) {
-            //     std::cout << " {" << key_value_data[(h - 1) * sz + i].first << ", " << key_value_data[(h - 1) * sz + i].second << "}";
-            // }
-            // std::cout << std::endl;
-            // std::cout << "cumulative_value[" << (h - 1) * sz << ":" << h * sz << "] =";
-            // for (int i = 0; i < sz; i++) {
-            //     std::cout << " " << cumulative_value[(h - 1) * sz + i];
-            // }
-            // std::cout << std::endl;
             t <<= 1;
         }
     }
 
     S _prod_section(int l, int r, std::optional<K> a, std::optional<K> b) const {
-        // std::cerr << "l = " << l%sz << ", r = " << r%sz << std::endl;
         S ret = e();
         if (a.has_value()) {
-            auto itr = std::lower_bound(cumulative_value.begin() + l, cumulative_value.begin() + r, a.value());
-            if (itr == cumulative_value.begin() + l) {
-                ret = op(ret, inv(*(--itr)));
-            } else {
-                ret = e();
+            int i = std::lower_bound(key_data.begin() + l, key_data.begin() + r, a.value()) - key_data.begin();
+            if (i != l) {
+                ret = inv(cumulative_value[i - 1]);
             }
-        } else {
-            ret = e();
         }
         if (b.has_value()) {
-            auto itr = std::upper_bound(cumulative_value.begin() + l, cumulative_value.begin() + r, b.value());
-            ret = op(ret, *(--itr));
+            int i = std::lower_bound(key_data.begin() + l, key_data.begin() + r, b.value()) - key_data.begin();
+            if (i != l) {
+                ret = op(ret, cumulative_value[i - 1]);
+            }
         } else {
-            ret = op(ret, *(cumulative_value.begin() + r - 1));
+            ret = op(ret, cumulative_value[r - 1]);
         }
         return ret;
     }
@@ -142,16 +155,16 @@ class MergeSortTree {
      */
     S prod(std::optional<int> l = std::nullopt, std::optional<int> r = std::nullopt, std::optional<K> a = std::nullopt, std::optional<K> b = std::nullopt) const {
         if (a.has_value() and b.has_value() and not(a.value() < b.value())) return e();
-        if (l >= r) return e();
+        if (l.has_value() and r.has_value() and l >= r) return e();
         return _prod(l.value_or(0), r.value_or(n), a, b);
     }
 
-    std::vector<std::pair<K, S>> to_vector() const {
-        std::vector<std::pair<K, S>> ret;
-        ret.reserve(n);
-        for (int i = 0; i < n; i++) {
-            ret.push_back(key_value_data[(height - 1) * sz + i]);
-        }
-        return ret;
-    }
+    // std::vector<std::pair<K, S>> to_vector() const {
+    //     std::vector<std::pair<K, S>> ret;
+    //     ret.reserve(n);
+    //     for (int i = 0; i < n; i++) {
+    //         ret.push_back(key_value_data[(height - 1) * sz + i]);
+    //     }
+    //     return ret;
+    // }
 };
