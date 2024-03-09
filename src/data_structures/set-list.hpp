f4085b6d1f30bdadd22f2aab70ac754542a15ed2
shogo314 <shg.kawasome@gmail.com>
2024-03-10 04:36:36 +0900

    stash

--- src/data_structures/set-list.hpp
+++ src/data_structures/set-list.hpp
@@ -1,20 +1,44 @@
+#pragma once
 #include <cassert>
+#include <cstddef>
 #include <initializer_list>
 #include <iterator>
+#include <list>
 #include <map>
+
 template <typename T>
 class SetList {
    public:
     using value_type = T;
+
+   private:
+    using base_type = std::list<value_type>;
+    using base_type_itr = typename std::list<value_type>::iterator;
+
+   public:
+    using reference = value_type&;
+    using const_reference = const value_type&;
+    using difference_type = std::ptrdiff_t;
+    using size_type = std::size_t;
+
     class iterator {
         SetList& setlist;
-        using value_type = T;
+        base_type_itr& itr;
+
+       public:
+        using difference_type = SetList::difference_type;
+        using value_type = iterator;
+        using pointer = SetList::value_type*;
+        using reference = SetList::value_type&;
+        using iterator_category = std::bidirectional_iterator_tag;
+        
     };
 
    private:
-    map<value_type, iterator> m;
+    std::map<value_type, iterator> m;
+    base_type ls;
     iterator _begin, _end;
 
    public:
-    SetList() : _begin() {}
+    SetList() : m{}, ls{}, _begin{*this, ls.end()}, _end{*this, ls.end()} {}
 };
